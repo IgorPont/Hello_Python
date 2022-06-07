25b936607ae3bb6b1322c7cd721ed6ab3a2fd6ff
igor_pont <pontigor11@gmail.com>
2022-06-07 17:17:51 +0300

    Add solution to problem #42 of homework #6

---
+++ homework_python/homework_6.py
@@ -0,0 +1,57 @@
+# ===Задача 42.===
+# Реализовать RLE алгоритм. Реализовать модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах
+
+def get_data_from_file(link):
+    data = open(link, 'r')
+    in_str = data.read()
+    data.close()
+    return in_str
+
+
+def push_encode_file(str, link):
+    with open(link, 'w') as data:
+        data.write(str)
+
+
+def rle_encode(data):
+    encode = ''
+    pr_char = ''
+    count = 1
+
+    if not data:
+        return ''
+    for char in data:
+        if char != pr_char:
+            if pr_char:
+                encode += str(count) + pr_char
+            count = 1
+            pr_char = char
+        else:
+            count += 1
+    encode += str(count) + pr_char
+    return encode
+
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+
+inp_link = '/Users/igor/Desktop/belive/study_projects/HelloPython/homework_python/files_homework_tasks/task_42_input_data.txt'
+out_link = '/Users/igor/Desktop/belive/study_projects/HelloPython/homework_python/files_homework_tasks/task_42_outputs_data.txt'
+
+inp_data = get_data_from_file(inp_link)
+code = rle_encode(inp_data)
+push_encode_file(code, out_link)
+
+print(inp_data)
+print(code)
+print(rle_decode(code))
